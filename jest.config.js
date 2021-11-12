2e12af9002953eafd656acf7dbd254215fa6f0ad
Yu Peng <xxxxx@smartmore.com>
2021-11-12 14:51:47 +0800

    feat: add jest-dom

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 module.exports = {
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
   moduleNameMapper: {
     '\\.svg': '<rootDir>/test/__mocks__/svgrMock.js',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
