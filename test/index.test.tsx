8f396a01d0ae67b35a36fe49cc2a7c98ff06011a
Yu Peng <xxxxx@smartmore.com>
2021-11-14 18:42:37 +0800

    fix(test): remove cleanup

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { cleanup, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Button from '../src';
-
-afterEach(cleanup);
 
 it('button should render', () => {
   const { queryByText } = render(<Button>click</Button>);
